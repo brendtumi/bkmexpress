8a1674cc1f656ddcd9ffe07f333b94638c2f6ff3
Tümay Çeber <tumayceber@gmail.com>
2017-04-24 17:44:01 +0300

    better ignore

--- src/bkmexpress.js
+++
@@ -1,9 +0,0 @@
-/**
- * @author  Tümay Çeber <tumayceber@gmail.com>
- * @link    https://github.com/brendtumi/bkmexpress
- * @license http://opensource.org/licenses/MIT
- */
-"use strict";
-exports.__esModule = true;
-var pkg = require("../package.json");
-exports.version = pkg.version;
