3b40fcb7ade5d433296b3321f460cbf585079819
Tümay Çeber <tumayceber@gmail.com>
2017-07-21 16:06:23 +0300

    Tested

--- lib/merchant/response/nonce/nonceResultResponse.d.ts
+++ lib/merchant/response/nonce/nonceResultResponse.d.ts
@@ -1,5 +1,5 @@
-import { PaymentResultResponse, PosData } from "../paymentResultResponse";
 import { RawBexResponse } from "../bexResponse";
+import { PaymentResultResponse, PosData } from "../paymentResultResponse";
 export declare class NonceResultResponse extends PaymentResultResponse {
     private error;
     constructor(obj?: RawBexResponse<PosData>);
